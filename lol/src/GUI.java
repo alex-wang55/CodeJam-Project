7ba36db084d3f9db1a8e9461ac01e7afdb182fd4
alex-wang55 <awang.ottawa@gmail.com>
2025-11-14 20:46:49 -0500

    poo

--- lol/src/GUI.java
+++ lol/src/GUI.java
@@ -1,22 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
-public class Interface {
-
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class GUI {
     public GUI() {
-        
+
         JFrame frame = new JFrame();
 
         JButton button = new JButton("Click me");
+        JLabel label = new JLabel("Number of clicks: 0");
 
-        
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
-        
+        panel.add(button);
+        panel.add(label);
+
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
         frame.setTitle("GUI");
         frame.pack();
         frame.setVisible(true);
