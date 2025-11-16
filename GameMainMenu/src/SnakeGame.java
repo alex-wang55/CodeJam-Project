8156e51daa036cd109499dd6a1a43a07a7776624
Jeffrey Dai <91475325+Zenderland@users.noreply.github.com>
2025-11-15 22:59:26 -0500

    Alex's sister smurfs him

--- GameMainMenu/src/SnakeGame.java
+++ GameMainMenu/src/SnakeGame.java
@@ -28,7 +28,7 @@ class GameFrame extends JFrame {
 class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 75;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
     static final int DELAY = 75;
 
